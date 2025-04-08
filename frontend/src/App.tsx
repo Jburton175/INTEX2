f173f00bc072ff9896d703e86d91b16a3ff481be
JonasButikofer <brojobutikofer@gmail.com>
2025-04-08 09:46:13 -0600

    cleaned some code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import HomePage from './pages/HomePage'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <HomePage />
-//     </>
-//   )
-// }
-
-// export default App
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from "./pages/HomePage";
-import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
