a4b8193a844ed9ec22a3016550fa03345f3e87ac
JBurton175 <jburton175@gmail.com>
2025-04-10 16:37:45 -0600

    added missed saves

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,22 +6,24 @@ export default defineConfig({
   server: {
     port: 3000,
     headers: {
-      'Content-Security-Policy':
+      "Content-Security-Policy":
         "default-src 'self'; " +
         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://www.googletagmanager.com; " +
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
-        "img-src 'self' data: https://blobintex.blob.core.windows.net https://cdn.builder.io;"+
+        "img-src 'self' data: https://blobintex.blob.core.windows.net https://cdn.builder.io; " +
         "font-src 'self' https://fonts.gstatic.com; " +
-        "connect-src 'self' https://localhost:5000 http://localhost:5000 http://localhost:4000 https://intexbackenddeployment-dzebbsdtf7fkapb7.westus2-01.azurewebsites.net; " +
-        "frame-src 'self';"
+        "connect-src 'self' https://localhost:5000 http://localhost:5000 http://localhost:4000 https://blobintex.blob.core.windows.net https://intexbackenddeployment-dzebbsdtf7fkapb7.westus2-01.azurewebsites.net; " +
+        "frame-src 'self';",
     },
+
     proxy: {
-      '/api': {
-        target: 'https://localhost:5000',
+      "/api": {
+        target:
+          "https://intexbackenddeployment-dzebbsdtf7fkapb7.westus2-01.azurewebsites.net",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
   build: {
     outDir: "dist",
