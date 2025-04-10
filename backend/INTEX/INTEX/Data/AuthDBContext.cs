e53047c1d15b0c0056c1e3a133a0fb1ac66283c2
JBurton175 <jburton175@gmail.com>
2025-04-10 10:00:49 -0600

    first auth video

---
+++ backend/INTEX/INTEX/Data/AuthDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace INTEX.Data
+{
+    public class AuthDBContext : IdentityDbContext<IdentityUser>
+    {
+
+        AuthDBContext(DbContextOptions<AuthDBContext> options):
+            base(options)
+    }
+}
